fab48ed7a6ccb840df1b29a7fcd554dce132f06f
Eslam Hesham <esl2mhesham@gmail.com>
2021-02-22 11:58:33 +0200

    reusable button

---
+++ src/components/button.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Button = ({ children }) => (
+  <button className="py-4 px-8 tracking-wider	 font-title font-bold rounded-lg bg-primary uppercase">
+    {children}
+  </button>
+)
+export default Button
