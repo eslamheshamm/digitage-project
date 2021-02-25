c9e6b6066584029299742f5bd6107fc8c96e8ba0
Eslam Hesham <esl2mhesham@gmail.com>
2021-02-25 17:09:55 +0200

    adding more validation to inputs

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -4,40 +4,53 @@ import { useForm } from "react-hook-form"
 const FormInputs = () => {
   const { register, handleSubmit, errors } = useForm()
   const onSubmit = data => console.log(data)
+
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col justify-between font-body"
+      className="flex flex-col justify-between font-body py-2"
       autoComplete="off"
+      id="form"
     >
-      {/* register your input into the hook by invoking the "register" function */}
-      <input
-        name="email"
-        type="email"
-        placeholder="Your email address"
-        ref={register({ required: true })}
-        className="p-8 border rounded-2xl mb-5 focus:ring-1 focus:ring-primary focus:outline-none"
-      />
+      <div className="mb-5">
+        <input
+          name="email"
+          type="email"
+          placeholder="Your email address"
+          ref={register({ required: true })}
+          className="p-8 border rounded-2xl focus:ring-1 focus:ring-primary focus:outline-none block w-full"
+        />
+        {errors.email && <span>This field is required</span>}
+      </div>
+      <div className="mb-5 w-full">
+        <input
+          name="name"
+          type="text"
+          placeholder="Your name / company’s name"
+          className="p-8 border rounded-2xl focus:ring-1 focus:ring-primary focus:outline-none block w-full"
+          ref={register({ required: true })}
+        />
+        {errors.name && <span>This field is required</span>}
+      </div>
+      <div className="w-full mb-8">
+        <textarea
+          placeholder="Your message"
+          name="message"
+          ref={register({ required: true, minLength: 15, maxLength: 150 })}
+          className="resize-none border rounded-2xl h-72 p-8  focus:ring-1 focus:ring-primary focus:outline-none w-full block"
+        />
+        {errors.message?.type === "required" && (
+          <span>This field is required</span>
+        )}
+        {errors.message?.type === "minLength" && (
+          <span>required to be more than 15 character</span>
+        )}
+        {errors.message?.type === "maxLength" && (
+          <span>Should be less than 150 character </span>
+        )}
 
-      <input
-        name="name"
-        type="text"
-        placeholder="Your name / company’s name"
-        className="p-8 border rounded-2xl mb-5 focus:ring-1 focus:ring-primary focus:outline-none"
-        ref={register({ required: true })}
-      />
-
-      {/* include validation with required or other standard HTML validation rules */}
-      {/* errors will return when field validation fails  */}
-      {errors.email && <span>This field is required</span>}
-      <textarea
-        placeholder="Your message"
-        name="message"
-        ref={register({ required: true, max: 200, min: 15 })}
-        className="resize-none border rounded-2xl h-72 p-8 mb-8 focus:ring-1 focus:ring-primary focus:outline-none"
-      />
-      {errors.email && <span>This field is required</span>}
-
+        {/* {errors.message && <span>This field is required</span>} */}
+      </div>
       <button
         type="submit"
         className="self-end py-5 px-20 rounded-2xl bg-primary font-semibold text-lg"
