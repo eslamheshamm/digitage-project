3592209ace3c20d21e781bd46989262deb5f7a9b
Eslam Hesham <esl2mhesham@gmail.com>
2021-03-06 03:02:42 +0200

    fix number

--- src/components/button.js
+++ src/components/button.js
@@ -2,7 +2,7 @@ import React from "react"
 const Button = ({ children }) => {
   return (
     <button className="py-4 px-8 tracking-wider text-black	 font-title font-bold rounded-lg bg-primary uppercase focus:outline-none">
-      <a href="https://wa.me/1555354072" target="_blank" rel="noreferrer">
+      <a href="https://wa.me/201555354072" target="_blank" rel="noreferrer">
         {" "}
         {children}
       </a>
