47ca3ad6d3f3e1734582afc4dcfc528f483e2828
Eslam Hesham <esl2mhesham@gmail.com>
2021-03-06 02:40:43 +0200

    fix dir

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "~components/seo"
 import Hero from "~components/cards/hero"
 import Cards from "~components/cards/cards"
 
-import Clients from "~components/clients"
+import Clients from "~components/clients/clients"
 import BannerSmall from "../components/banners/bannersmall"
 import BannerThree from "../components/banners/banner-three"
 import Form from "../components/form"
